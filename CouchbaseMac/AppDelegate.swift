129c9028d55fbb99e7cdbfc7917a1410d3ccf45e
srinivas <srinivas@codezyng.com>
2020-06-10 21:51:08 +0530

    Initial Commit

---
+++ CouchbaseMac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CouchbaseMac
+//
+//  Created by Srinivas Prabhu G on 10/06/20.
+//  Copyright © 2020 Srinivas Prabhu G. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CouchbaseMac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CouchbaseMac
+//
+//  Created by Srinivas Prabhu G on 10/06/20.
+//  Copyright © 2020 Srinivas Prabhu G. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
